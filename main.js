a2a33bc7008958ed28fc79656889d07b2d5ed9a4
Angelo Rodrigues <angelorodrigues318@gmail.com>
2022-06-28 10:18:28 -0300

    Finished the Calculator

---
+++ main.js
@@ -0,0 +1,16 @@
+function calcular(tipo, valor){
+    
+    if(tipo === 'acao') {
+        if(valor === 'c'){
+            document.getElementById('resultado').value = '';
+        } else if(valor === '/' || valor === '*' || valor === '-' || valor === '+' || valor === '.') {
+            document.getElementById('resultado').value += valor;    
+        } else if(valor === '=') {
+            var valorResultado = eval(document.getElementById('resultado').value);
+            document.getElementById('resultado').value = valorResultado;
+        }
+    } else if(tipo === 'valor') {
+        document.getElementById('resultado').value += valor;
+    }
+
+}
